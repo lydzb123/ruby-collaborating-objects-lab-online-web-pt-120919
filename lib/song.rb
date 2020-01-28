d5d00a252f2dcd5c39441cf7949b75f90602e879
Lydia B <lydiabaik@gmail.com>
2020-01-28 16:57:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,15 +15,16 @@ class Song
   def self.new_by_filename(filename)
     song_name = filename.split(" - ")[1]
     artist_name = filename.split(" - ")[0]
-
     if
     new_instance = self.new(song_name)
     new_instance.artist = Artist.new(artist_name)
-  end
+    end
+end
 
   def artist_name=(artistname)
     self.artist = Artist.new(artistname)
   end
 #create songs fiven each filenames
 #send artists name "a string" to the Artist class
+end
 end
